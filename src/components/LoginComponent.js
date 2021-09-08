c383659c9a378f1a0dcffcc5398b5a8c74a2bcbd
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2021-09-08 20:49:43 +0200

    nav

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -2,10 +2,29 @@ import React from 'react'
 
 function LoginComponent() {
     return (
-      <div>
-        <form>
-          <input type="email" />
-        </form>
+      <div className="row mt-5">
+        <div className="col"></div>
+        <div className="col">
+          <form className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              placeholder="escribe tu email"
+            />
+
+            <input
+              className="form-control mt-4"
+              type="password"
+              placeholder="escribe tu password"
+            />
+            <input
+              className="btn btn-dark btn-block mt-4"
+              type="submit"
+              value="Login Usuario  "
+            />
+          </form>
+        </div>
+        <div className="col"></div>
       </div>
     );
 }
--- src/router/Rutas.js
+++ src/router/Rutas.js
@@ -10,37 +10,42 @@ const Rutas = () => {
     return (
       <div>
         <Router>
-          <nav>
+          <nav className="navbar navbar-expan-lg navbar-dark bg-dark">
             <ul className="nav">
-              <li>
-                <Link to="/">Login</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Login
+                </Link>
               </li>
               <li>
-                <Link to="/Administrador">Administrador</Link>
+                <Link className="nav-link" to="/Administrador">
+                  Administrador
+                </Link>
               </li>
               <li>
-                <Link to="/Trabajador_1">Trabajador 1º</Link>
+                <Link className="nav-link" to="/Trabajador_1">
+                  Trabajador 1º
+                </Link>
               </li>
               <li>
-                <Link to="/Trabajador_2">Trabajador 2º</Link>
+                <Link className="nav-link" to="/Trabajador_2">
+                  Trabajador 2º
+                </Link>
               </li>
             </ul>
           </nav>
 
           <Switch>
             <Route exact path="/" component="Login">
-              
               <Login />
             </Route>
-            <Route exact path="/Administrador" component="Administrador">
-              <Administrador />
-            </Route>
-            <Route exact path="/Trabajador_1" component="Trabajador_1">
-              <Trabajador_1 />
-            </Route>
-            <Route exact path="/Trabajador_2" component="Trabajador_2">
-              <Trabajador_2 />
-            </Route>
+            <Route
+              exact
+              path="/Administrador"
+              component={Administrador}
+            ></Route>
+            <Route exact path="/Trabajador_1" component={Trabajador_1}></Route>
+            <Route exact path="/Trabajador_2" component={Trabajador_2}></Route>
           </Switch>
         </Router>
       </div>
