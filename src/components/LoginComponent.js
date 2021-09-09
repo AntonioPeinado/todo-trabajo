59de189c8ce036a1305163068eed3cd9af370f22
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2021-09-09 21:08:04 +0200

    terminar login y nav

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../style/nav.css'
 function LoginComponent() {
     return (
       <div className="row mt-5">
@@ -7,7 +7,7 @@ function LoginComponent() {
         <div className="col">
           <form className="form-group">
             <input
-              className="form-control"
+              className="form-control mt-4"
               type="email"
               placeholder="escribe tu email"
             />
@@ -18,7 +18,7 @@ function LoginComponent() {
               placeholder="escribe tu password"
             />
             <input
-              className="btn btn-dark btn-block mt-4"
+              className="btn btn-lg btn-dark btn-block mt-4 "
               type="submit"
               value="Login Usuario  "
             />
--- src/router/Rutas.js
+++ src/router/Rutas.js
@@ -1,55 +1,53 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Login from "../pages/Login";
 import Administrador from "../pages/Administrador";
 import Trabajador_1 from "../pages/Trabajador_1";
 import Trabajador_2 from "../pages/Trabajador_2";
-
+import "../style/rutas.css";
 
 const Rutas = () => {
-    return (
-      <div>
-        <Router>
-          <nav className="navbar navbar-expan-lg navbar-dark bg-dark">
-            <ul className="nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-link" to="/Administrador">
-                  Administrador
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-link" to="/Trabajador_1">
-                  Trabajador 1º
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-link" to="/Trabajador_2">
-                  Trabajador 2º
-                </Link>
-              </li>
-            </ul>
-          </nav>
+  return (
+    <div>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-2">
+          <ul className="navbar-nav mr-auto ">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link " to="/Administrador">
+                Administrador
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/Trabajador_1">
+                Trabajador 1º
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/Trabajador_2">
+                Trabajador 2º
+              </Link>
+            </li>
+          </ul>
+          <div className="col"></div>
+          <button className="btn btn-danger  ">Cerrar sesión</button>
+        </nav>
 
-          <Switch>
-            <Route exact path="/" component="Login">
-              <Login />
-            </Route>
-            <Route
-              exact
-              path="/Administrador"
-              component={Administrador}
-            ></Route>
-            <Route exact path="/Trabajador_1" component={Trabajador_1}></Route>
-            <Route exact path="/Trabajador_2" component={Trabajador_2}></Route>
-          </Switch>
-        </Router>
-      </div>
-    );
-}
+        <Switch>
+          <Route exact path="/" component="Login">
+            <Login />
+          </Route>
+          <Route exact path="/Administrador" component={Administrador}></Route>
+          <Route exact path="/Trabajador_1" component={Trabajador_1}></Route>
+          <Route exact path="/Trabajador_2" component={Trabajador_2}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
-export default Rutas
+export default Rutas;
